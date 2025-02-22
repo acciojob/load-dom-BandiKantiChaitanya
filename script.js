56d26f248fe808dab633c4829c30a42f4231a4d5
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-22 13:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", () => {
-	document.querySelector('body').innerHTML+=`<h1>DOM Load Sucess</h1>`
+	document.querySelector('body').innerHTML+=`DOM load success`
 });
